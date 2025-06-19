f632d25c3e3389df98de5ab8a4362959525252bf
jacob zhang <jwzhang0610@gmail.com>
2025-06-19 00:12:29 -0500

    try running local mcp server as one of subagents 2

--- manager/sub_agents/db_mcp_client_agent/agent.py
+++ manager/sub_agents/db_mcp_client_agent/agent.py
@@ -10,7 +10,7 @@ from .prompt import DB_MCP_PROMPT
 PATH_TO_YOUR_MCP_SERVER_SCRIPT = str((Path(__file__).parent / "server.py").resolve())
 
 
-db_mcp_client_agent = LlmAgent(
+root_agent = LlmAgent(
     model="gemini-2.0-flash",
     name="db_mcp_client_agent",
     instruction=DB_MCP_PROMPT,
